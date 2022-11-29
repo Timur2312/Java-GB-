76b34fbfb26b036ed33553ace6784f51beca6968
Timur <arbo1234@yandex.ru>
2022-11-29 22:40:41 +0300

    Сделала первое задание

---
+++ Lesson_1/Seminar_1/HomeWork/DZ1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+// Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+/**
+ * DZ1
+ */
+public class DZ1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String n = in.nextLine();
+        if (CheckNumber(n) == true) {
+            double number = Double.parseDouble(n);
+            double result = TriangularNumber(number);
+            System.out.println(result);
+        }
+        else {
+            System.out.println("Вы ввели не число");
+        }
+     
+    }
+    static double TriangularNumber(double number) {
+        double result = 1d / 2 * number * (number + 1);
+        return result;
+    }
+    static Boolean CheckNumber(String value){
+
+        try {
+            Double.parseDouble(value);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
